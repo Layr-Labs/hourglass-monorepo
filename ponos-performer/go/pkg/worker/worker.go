d81ae83c1063983b996d477db6cb6ce65b6fa18b
Sean McGary <sean@seanmcgary.com>
2025-04-17 10:27:21 -0500

    feat: include a ValidateTask() function as part of the interface

--- ponos-performer/go/pkg/worker/worker.go
+++ ponos-performer/go/pkg/worker/worker.go
@@ -4,4 +4,5 @@ import "github.com/Layr-Labs/hourglass-monorepo/ponos-performer/go/pkg/task"
 
 type IWorker interface {
 	HandleTask(task *task.Task) (*task.TaskResult, error)
+	ValidateTask(task *task.Task) error
 }
