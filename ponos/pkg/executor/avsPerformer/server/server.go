4f03b6f37e636a2d3ee6609a34e9904dccf28ad0
Sean McGary <sean@seanmcgary.com>
2025-04-21 10:33:06 -0500

    fix wait for container port

--- ponos/pkg/executor/avsPerformer/server/server.go
+++ ponos/pkg/executor/avsPerformer/server/server.go
@@ -152,6 +152,23 @@ func (aps *AvsPerformerServer) waitForRunning(ctx context.Context, dockerClient 
 		}
 
 		if info.State.Running {
+			containerInfo, err := dockerClient.ContainerInspect(ctx, containerId)
+			if err != nil {
+				return false, err
+			}
+			portMap, ok := containerInfo.NetworkSettings.Ports[containerPort]
+			if !ok {
+				aps.logger.Sugar().Infow("Port map not yet available", zap.String("containerId", containerId))
+				continue
+			}
+			if len(portMap) == 0 {
+				aps.logger.Sugar().Infow("Port map is empty", zap.String("containerId", containerId))
+				continue
+			}
+			aps.logger.Sugar().Infow("Container is running with port exposed",
+				zap.String("containerId", containerId),
+				zap.String("exposedPort", portMap[0].HostPort),
+			)
 			return true, nil
 		}
 
