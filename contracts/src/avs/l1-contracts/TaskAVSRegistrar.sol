a7fc986e6073fc6c9e584107d178c75cc98e9674
0xrajath <rajathalex@gmail.com>
2025-04-30 17:42:47 -0400

    feat: allocation manager in registrar

--- contracts/src/avs/l1-contracts/TaskAVSRegistrar.sol
+++ contracts/src/avs/l1-contracts/TaskAVSRegistrar.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.27;
 
 import {TaskAVSRegistrarStorage} from "src/avs/l1-contracts/TaskAVSRegistrarStorage.sol";
+import {IAllocationManager} from
+    "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 
 contract TaskAVSRegistrar is TaskAVSRegistrarStorage {
-    constructor(
-        address avs
-    ) TaskAVSRegistrarStorage(avs) {}
+    constructor(address avs, IAllocationManager allocationManager) TaskAVSRegistrarStorage(avs, allocationManager) {}
 
     function registerOperator(
         address operator,
--- contracts/src/avs/l1-contracts/TaskAVSRegistrarStorage.sol
+++ contracts/src/avs/l1-contracts/TaskAVSRegistrarStorage.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.27;
 
 import {ITaskAVSRegistrar} from "src/interfaces/ITaskAVSRegistrar.sol";
+import {IAllocationManager} from
+    "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 
 abstract contract TaskAVSRegistrarStorage is ITaskAVSRegistrar {
     /// @notice The avs address for this AVS (used for UAM integration in EigenLayer)
@@ -9,9 +11,11 @@ abstract contract TaskAVSRegistrarStorage is ITaskAVSRegistrar {
     /// This value should only be set once.
     address public immutable AVS;
 
-    constructor(
-        address avs
-    ) {
+    /// @notice The AllocationManager that tracks OperatorSets and Slashing in EigenLayer
+    IAllocationManager public immutable ALLOCATION_MANAGER;
+
+    constructor(address avs, IAllocationManager allocationManager) {
         AVS = avs;
+        ALLOCATION_MANAGER = allocationManager;
     }
 }
