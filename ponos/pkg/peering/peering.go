efb81a4c8c0f81d8aedfbc80afc37e5d803f72f7
Brandon Chatham <brandon.chatham@eigenlabs.org>
2025-05-02 15:42:34 -0700

    Adding event parsing for operator registration. Also fixing a small race condition in peering info.

--- ponos/pkg/peering/peering.go
+++ ponos/pkg/peering/peering.go
@@ -9,6 +9,17 @@ type OperatorPeerInfo struct {
 	OperatorSetIds  []uint32
 }
 
+func (opi *OperatorPeerInfo) Copy() *OperatorPeerInfo {
+	operatorSetIds := make([]uint32, len(opi.OperatorSetIds))
+	copy(operatorSetIds, opi.OperatorSetIds)
+	return &OperatorPeerInfo{
+		NetworkAddress:  opi.NetworkAddress,
+		PublicKey:       opi.PublicKey,
+		OperatorAddress: opi.OperatorAddress,
+		OperatorSetIds:  operatorSetIds,
+	}
+}
+
 type IPeeringDataFetcher interface {
 	ListExecutorOperators(ctx context.Context, avsAddress string) ([]*OperatorPeerInfo, error)
 	ListAggregatorOperators(ctx context.Context, avsAddress string) ([]*OperatorPeerInfo, error)
