11a76930743c932724add6a07b247a94f970b459
0xrajath <rajathalex@gmail.com>
2025-05-01 22:18:50 -0400

    chore: forge fmt

--- contracts/script/DeployAndRegisterAVS.s.sol
+++ contracts/script/DeployAndRegisterAVS.s.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.8.27;
 
 import {Script, console} from "forge-std/Script.sol";
 
-import {IAllocationManager} from "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
+import {IAllocationManager} from
+    "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 import {IAVSRegistrar} from "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IAVSRegistrar.sol";
-import {IPermissionController} from "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IPermissionController.sol";
+import {IPermissionController} from
+    "@eigenlayer-middleware/lib/eigenlayer-contracts/src/contracts/interfaces/IPermissionController.sol";
 
 import {TaskAVSRegistrar} from "src/avs/l1-contracts/TaskAVSRegistrar.sol";
-
 
 contract DeployAndRegisterAVS is Script {
     // Eigenlayer Core Contracts
     IAllocationManager public ALLOCATION_MANAGER = IAllocationManager(0x948a420b8CC1d6BFd0B6087C2E7c344a2CD0bc39);
-    IPermissionController public PERMISSION_CONTROLLER = IPermissionController(0x25E5F8B1E7aDf44518d35D5B2271f114e081f0E5);
+    IPermissionController public PERMISSION_CONTROLLER =
+        IPermissionController(0x25E5F8B1E7aDf44518d35D5B2271f114e081f0E5);
 
     function setUp() public {}
 
-    function run(string memory metadataURI) public {
+    function run(
+        string memory metadataURI
+    ) public {
         // Load the private key from the environment variable
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOYER");
         uint256 avsPrivateKey = vm.envUint("PRIVATE_KEY_AVS");
