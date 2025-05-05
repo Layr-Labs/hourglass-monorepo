2334363007ada0ca0d64f5ce7f2d629ee434f337
Sean McGary <sean@seanmcgary.com>
2025-05-05 14:30:00 -0500

    build contracts to test against

--- ponos/cmd/mailbox/main.go
+++ ponos/cmd/mailbox/main.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	privateKey             = "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
-	mailboxContractAddress = "0x74E7CF978C61685dB8527086CD66316Ce7aF295c"
+	privateKey             = "5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
+	mailboxContractAddress = "0x7306a649b451ae08781108445425bd4e8acf1e00"
 )
 
 func main() {
@@ -31,7 +31,7 @@ func main() {
 
 	cc, err := caller.NewContractCaller(&caller.ContractCallerConfig{
 		PrivateKey:          privateKey,
-		AVSRegistrarAddress: "0x1234567890abcdef1234567890abcdef12345678",
+		AVSRegistrarAddress: "0xf4c5c29b14f0237131f7510a51684c8191f98e06",
 		TaskMailboxAddress:  mailboxContractAddress,
 	}, ethCaller, l)
 	if err != nil {
@@ -40,7 +40,7 @@ func main() {
 
 	payloadJsonBytes := []byte(`{ "numberToBeSquared": 4 }`)
 
-	receipt, err := cc.PublishMessageToInbox(context.Background(), "0x1234", 1, payloadJsonBytes)
+	receipt, err := cc.PublishMessageToInbox(context.Background(), "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", 1, payloadJsonBytes)
 	if err != nil {
 		panic(err)
 	}
