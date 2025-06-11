0698c8b8cd78fc46ef94155532226cf9a7015c8f
0xrajath <rajathalex@gmail.com>
2025-06-11 15:48:33 -0400

    feat: TaskAVSRegistrarBaseStorage

--- contracts/src/avs/TaskAVSRegistrarBase.sol
+++ contracts/src/avs/TaskAVSRegistrarBase.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
-import {AVSRegistrarWithSocket} from "@eigenlayer-middleware/src/middlewareV2/registrar/presets/AVSRegistrarWithSocket.sol";
 import {IAllocationManager} from "@eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 import {IKeyRegistrar} from "@eigenlayer-contracts/src/contracts/interfaces/IKeyRegistrar.sol";
+
+import {TaskAVSRegistrarBaseStorage} from "./TaskAVSRegistrarBaseStorage.sol";
 
 /**
  * @title TaskAVSRegistrarBase
  * @author Layr Labs, Inc.
  * @notice Abstract contract that extends AVSRegistrarWithSocket for task-based AVSs
- * @dev This is a minimal wrapper around AVSRegistrarWithSocket
+ * @dev This is a minimal wrapper that inherits from TaskAVSRegistrarBaseStorage
  */
-abstract contract TaskAVSRegistrarBase is AVSRegistrarWithSocket {
+abstract contract TaskAVSRegistrarBase is TaskAVSRegistrarBaseStorage {
     /**
      * @notice Constructs the TaskAVSRegistrarBase contract
      * @param _avs The address of the AVS
@@ -22,5 +23,5 @@ abstract contract TaskAVSRegistrarBase is AVSRegistrarWithSocket {
         address _avs,
         IAllocationManager _allocationManager,
         IKeyRegistrar _keyRegistrar
-    ) AVSRegistrarWithSocket(_avs, _allocationManager, _keyRegistrar) {}
+    ) TaskAVSRegistrarBaseStorage(_avs, _allocationManager, _keyRegistrar) {}
 } 
