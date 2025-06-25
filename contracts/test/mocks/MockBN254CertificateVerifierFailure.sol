1b288da03c695286bb0ff684b732d30572faf449
0xrajath <rajathalex@gmail.com>
2025-06-25 19:58:54 -0400

    chore: updated eigenlayer-middleware dependency

--- contracts/test/mocks/MockBN254CertificateVerifierFailure.sol
+++ contracts/test/mocks/MockBN254CertificateVerifierFailure.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
-import {IBN254CertificateVerifier} from "@eigenlayer-contracts/src/contracts/interfaces/IBN254CertificateVerifier.sol";
+import {
+    IBN254CertificateVerifier,
+    IBN254CertificateVerifierTypes
+} from "@eigenlayer-contracts/src/contracts/interfaces/IBN254CertificateVerifier.sol";
+import {IBN254TableCalculatorTypes} from "@eigenlayer-contracts/src/contracts/interfaces/IBN254TableCalculator.sol";
 import {OperatorSet} from "@eigenlayer-contracts/src/contracts/libraries/OperatorSetLib.sol";
+import {BN254} from "@eigenlayer-contracts/src/contracts/libraries/BN254.sol";
 
 /**
  * @title MockBN254CertificateVerifierFailure
@@ -13,7 +18,7 @@ contract MockBN254CertificateVerifierFailure is IBN254CertificateVerifier {
     function updateOperatorTable(
         OperatorSet calldata, /*operatorSet*/
         uint32, /*referenceTimestamp*/
-        BN254OperatorSetInfo memory, /*operatorSetInfo*/
+        IBN254TableCalculatorTypes.BN254OperatorSetInfo memory, /*operatorSetInfo*/
         OperatorSetConfig calldata /*operatorSetConfig*/
     ) external pure {}
 
@@ -69,5 +74,44 @@ contract MockBN254CertificateVerifierFailure is IBN254CertificateVerifier {
         OperatorSet memory /*operatorSet*/
     ) external pure returns (uint32) {
         return 86_400;
+    }
+
+    function trySignatureVerification(
+        bytes32, /*msgHash*/
+        BN254.G1Point memory, /*aggPubkey*/
+        BN254.G2Point memory, /*apkG2*/
+        BN254.G1Point memory /*signature*/
+    ) external pure returns (bool pairingSuccessful, bool signatureValid) {
+        return (true, false); // Pairing succeeds but signature is invalid
+    }
+
+    function getNonsignerOperatorInfo(
+        OperatorSet memory, /*operatorSet*/
+        uint32, /*referenceTimestamp*/
+        uint256 /*operatorIndex*/
+    ) external pure returns (IBN254TableCalculatorTypes.BN254OperatorInfo memory) {
+        uint256[] memory weights = new uint256[](0);
+        return IBN254TableCalculatorTypes.BN254OperatorInfo({pubkey: BN254.G1Point(0, 0), weights: weights});
+    }
+
+    function isNonsignerCached(
+        OperatorSet memory, /*operatorSet*/
+        uint32, /*referenceTimestamp*/
+        uint256 /*operatorIndex*/
+    ) external pure returns (bool) {
+        return false;
+    }
+
+    function getOperatorSetInfo(
+        OperatorSet memory, /*operatorSet*/
+        uint32 /*referenceTimestamp*/
+    ) external pure returns (IBN254TableCalculatorTypes.BN254OperatorSetInfo memory) {
+        uint256[] memory totalWeights = new uint256[](0);
+        return IBN254TableCalculatorTypes.BN254OperatorSetInfo({
+            operatorInfoTreeRoot: bytes32(0),
+            numOperators: 0,
+            aggregatePubkey: BN254.G1Point(0, 0),
+            totalWeights: totalWeights
+        });
     }
 }
